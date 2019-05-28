cab1ce9ada7324057d1496f7dcf934d1af98d30c
Tang <18505000351@163.com>
2019-05-28 11:06:27 +0800

    Colloctor第一次commit
    包含前、后端代码

---
+++ Collector-Server/mongoose.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true },  function(err){
+//     if(err){
+//         console.log('连接失败');
+//     }else{
+//         console.log('连接成功');
+//     }
+// }) //服务器
+mongoose.connect('mongodb://controller:cccon@problem-kid.top:20006/app', { useNewUrlParser: true },  function(err){
+    if(err){
+        console.log('连接失败');
+    }else{
+        console.log('连接成功');
+    }
+}) //服务器
+mongoose.Promise = global.Promise
+module.exports = mongoose
